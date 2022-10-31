9232a8c3bd29209479709dcb2b36234a10dd40cc
Esther Morris <EstherMorris775@gmail.com>
2022-10-31 13:17:48 -0400

    Book Controller now creates, updates and deletes the associated Book Categories table

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,23 +26,39 @@ class BooksController < ApplicationController
       author_id: params[:author_id],
     )
     book.save
-    #Will need to add if books belog to multiple categories
-    category = Category.find_by(name: params[:category])
-    book_catagory = BookCategory.new(book_id: book.id, category_id: category.id)
-    book_catagory.save
+    params[:categories].each do |category|
+      find_category = Category.find_by(name: category)
+      book_catagory = BookCategory.new(book_id: book.id, category_id: find_category.id)
+      book_catagory.save
+    end
 
     render json: book.as_json
   end
 
   def update
     book = Book.find_by(id: params[:id])
+    book.stock = params[:stock] || book.stock
+    book.author_id = params[:author_id] || book.author_id
+    book.available = params[:available] || book.available
+    book.title = params[:title] || book.title
 
-    Bookbook.stock = params[:stock] || book.stock
-    book.author_iBookd = params[:author_id] || book.author_id
-    book.availablBooke = params[:available] || book.available
-    Bookbook.title = params[:title] || book.title
+    book.save
 
-    Bookbook.save
+    #update category
+    if BookCategory.find_by(book_id: book.id) #if the book has at least one category already assigned
+      bc = BookCategory.where(book_id: book.id)
+      #loop through each category
+      bc.each do |category|
+        #delete each category
+        category.destroy
+      end
+    end
+
+    params[:categories].each do |category|
+      find_category = Category.find_by(name: category)
+      book_category = BookCategory.create(book_id: book.id, category_id: find_category.id)
+      book_category.save
+    end
     render json: book.as_json
   end
 
@@ -50,6 +66,9 @@ class BooksController < ApplicationController
     book = Book.find_by(id: params[:id])
 
     book.destroy
+    BookCategory.where(book_id: book.id).each do |book|
+      book.destroy
+    end
 
     render json: { message: "You have removed #{book.title.as_json} from circulation at the Library" }
   end
